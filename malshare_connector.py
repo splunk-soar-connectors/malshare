d1e08da321ea1cdd40d797827878b41963706f98
Shiying Tu <shiyingtu@gmail.com>
2022-03-03 11:57:53 -0800

    Fix semgrep tests

--- malshare_connector.py
+++ malshare_connector.py
@@ -374,7 +374,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) < 2:
         print("No test json specified as input")
-        exit(0)
+        sys.exit(0)
 
     with open(sys.argv[1]) as f:
         in_json = f.read()
@@ -386,4 +386,4 @@ if __name__ == '__main__':
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
